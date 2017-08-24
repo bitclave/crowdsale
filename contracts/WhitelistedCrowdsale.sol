13f095a7ed3cd4e2ca27d116bc2ca201d90fa9b2
Ivan Yurin <yurin.ivan@gmail.com>
2017-08-24 22:05:49 +0300

    Add WhitelistedCrowdsale

---
+++ contracts/WhitelistedCrowdsale.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.11;
+
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+
+
+/**
+ * @title WhitelistedCrowdsale
+ * @dev Extension of Crowsdale where an owner can whitelist addresses
+ * which can buy in crowdsale before it opens to the public 
+ */
+contract WhitelistedCrowdsale is Crowdsale, Ownable {
+    using SafeMath for uint256;
+
+    // list of addresses that can purchase before crowdsale opens
+    mapping (address => bool) public whitelist;
+
+    function addToWhitelist(address buyer) public onlyOwner {
+        require(buyer != 0x0);
+        whitelist[buyer] = true; 
+    }
+
+    function removeFromWhitelist(address buyer) public onlyOwner {
+        require(buyer != 0x0);
+        whitelist[buyer] = false; 
+    }
+
+    // @return true if buyer is whitelisted
+    function isWhitelisted(address buyer) public constant returns (bool) {
+        return whitelist[buyer];
+    }
+
+    // overriding Crowdsale#validPurchase to add whitelist logic
+    // @return true if buyers can buy at the moment
+    function validPurchase() internal constant returns (bool) {
+        // [TODO] issue with overriding and associativity of logical operators
+        return super.validPurchase() || (!hasEnded() && isWhitelisted(msg.sender)); 
+    }
+}
