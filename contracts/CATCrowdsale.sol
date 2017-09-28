8532a03db21454982b6264692d3930f12f81f386
Anton Bukov <k06aaa@gmail.com>
2017-09-28 16:48:18 +0300

    Working on crowdsale contract

--- contracts/CATCrowdsale.sol
+++ contracts/CATCrowdsale.sol
@@ -1,68 +1,90 @@
 pragma solidity ^0.4.11;
 
-import "zeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
-import "zeppelin-solidity/contracts/crowdsale/FinalizableCrowdsale.sol";
-import "./crowdsale/CappedCrowdsale.sol";
+import "zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./CAToken.sol";
 
 
-contract CATCrowdsale is FinalizableCrowdsale, CappedCrowdsale {
+contract CATCrowdsale is CappedCrowdsale, Ownable {
+
+    // Constants
     uint256 public constant DECIMALS = 18;
-    uint256 public constant CAP = 2 * (10**9) * 10**DECIMALS;  // 2MM CAT
+    uint256 public constant CAP = 2 * (10**9) * 10**DECIMALS;                // 2MM CAT
+    uint256 public constant BITCLAVE_AMOUNT = 1 * (10**9) * 10**DECIMALS;    // 1MM CAT
+    uint256 public constant PRESALED_AMOUNT = 150 * (10**6) * 10**DECIMALS;  // 150M CAT
 
+    // amount of raised money in BTC
+    uint256 public btcRaised;
+    // amount of raised money in QTUM
+    uint256 public qtumRaised;
+
+    // Events
+    event TokenPurchaseBTC(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+    event TokenPurchaseQTUM(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
     event WalletChange(address wallet);
+    event RateUpdate(uint256 rate);
 
+    // Constructor
     function CATCrowdsale(
-        uint256 _startBlock,
-        uint256 _endBlock,
-        uint256 _initialRate,
-        uint256 _endRate,
-        uint256 _preferentialRate,
-        uint256 _ethRate,
-        address _wallet
+        address _wallet,
+        address _bitClaveWallet,
+        address _presaledWallet,
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _rate
     )
         CappedCrowdsale(CAP)
-        WhitelistedCrowdsale()
-        FinalizableCrowdsale()
-        CrowdsaleRate(_initialRate, _endRate, _preferentialRate, _ethRate)
-        Crowdsale(_startBlock, _endBlock, _initialRate, _wallet)
+        Crowdsale(_startTime, _endTime, _rate, _wallet)
     {
-        CAToken(token).pause();
+        mintTokens(_bitClaveWallet, BITCLAVE_AMOUNT);
+        mintTokens(_presaledWallet, PRESALED_AMOUNT);
     }
+
+    // Overrided methods
 
     function createTokenContract() internal returns(MintableToken) {
         return new CAToken();
     }
 
-    // low level token purchase function
-    function buyTokens(address beneficiary) payable {
+    // Added methods
+
+    function mintTokens(address beneficiary, uint256 tokens) internal onlyOwner {
         require(beneficiary != 0x0);
-        require(validPurchase());
-
-        uint256 weiAmount = msg.value;
-        uint256 tokenAmount = getAmountToken(weiAmount);
-
-        weiRaised = weiRaised.add(weiAmount);
-
-        token.mint(beneficiary, tokenAmount);
-        TokenPurchase(msg.sender, beneficiary, weiAmount, tokenAmount);
-
-        forwardFunds();
+        token.mint(beneficiary, tokens);
     }
 
-    function setWallet(address _wallet) onlyOwner public {
+    function mintTokensForBTC(address beneficiary, uint256 value, uint256 tokens) external onlyOwner {
+        btcRaised += value;
+        mintTokens(beneficiary, tokens);
+        TokenPurchaseBTC(
+            msg.sender,
+            beneficiary,
+            value,
+            tokens
+        );
+    }
+
+    function mintTokensForQTUM(address beneficiary, uint256 value, uint256 tokens) external onlyOwner {
+        qtumRaised += value;
+        mintTokens(beneficiary, tokens);
+        TokenPurchaseQTUM(
+            msg.sender,
+            beneficiary,
+            value,
+            tokens
+        );
+    }
+
+    function setWallet(address _wallet) external onlyOwner {
         require(_wallet != 0x0);
         wallet = _wallet;
         WalletChange(_wallet);
     }
 
-    function unpauseToken() onlyOwner {
-        require(isFinalized);
-        CAToken(token).unpause();
+    function setRate(uint256 _rate) external onlyOwner {
+        require(_rate != 0x0);
+        rate = _rate;
+        RateUpdate(_rate);
     }
 
-    function pauseToken() onlyOwner {
-        require(isFinalized);
-        CAToken(token).pause();
-    }
 }
--- contracts/CAToken.sol
+++ contracts/CAToken.sol
@@ -1,11 +1,15 @@
 pragma solidity ^0.4.11;
 
-import "zeppelin-solidity/contracts/token/PausableToken.sol";
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
 import "zeppelin-solidity/contracts/token/MintableToken.sol";
 
 
-contract CAToken is PausableToken, MintableToken {
+contract CAToken is MintableToken {
+    
+    // Metadata
     string public constant symbol = "CAT";
     string public constant name = "Consumer Activity Token";
     uint256 public constant decimals = 18;
+    string public constant version = "1.0";
+
 }
