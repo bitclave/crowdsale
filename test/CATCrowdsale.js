625633c0aaf0a340c414d161e159e6522e99ac4c
Anton Bukov <k06aaa@gmail.com>
2017-10-20 18:48:42 +0300

    Add test to check failure of minting too many tokens

--- test/CATCrowdsale.js
+++ test/CATCrowdsale.js
@@ -47,7 +47,7 @@ contract('CATCrowdsale', function ([_, wallet, remainingsWallet, bitClaveWallet,
         });
     }
 
-    makeSuite('setters failure', async function () {
+    makeSuite('mint failure', async function () {
 
         it('should failure to mint tokens to 0', async function () {
             await crowdsale.mintTokens(0, 1).should.be.rejectedWith(EVMThrow);
@@ -56,6 +56,14 @@ contract('CATCrowdsale', function ([_, wallet, remainingsWallet, bitClaveWallet,
         it('should failure to mint 0 tokens', async function () {
             await crowdsale.mintTokens(wallet, 0).should.be.rejectedWith(EVMThrow);
         })
+
+        it('should failure to mint to many tokens', async function () {
+            await crowdsale.mintTokens(wallet, (new BigNumber((10**9) * (10**decimals))).add(1)).should.be.rejectedWith(EVMThrow);
+        })
+
+    })
+
+    makeSuite('setters failure', async function () {
 
         it('should failure to set 0 to rate', async function () {
             await crowdsale.setRate(0).should.be.rejectedWith(EVMThrow);
