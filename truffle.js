52cc80c1a565da2315d631867c51b1a3a46a1837
SprinterGameDev <sprinter@ozersk.com>
2017-10-26 16:27:02 +0500

    remove private key of testrpc connection.

--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ require('babel-polyfill');
 const Provider = require('./helpers/Provider');
 const ProviderRopsten = Provider.createRopstenNetwork("type here your private key from owner address");
 const ProviderMain = Provider.createMainNetwork("type here your private key from owner address");
-const ProviderTestRpc = Provider.createTestRpcNetwork("2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200");
+const ProviderTestRpc = Provider.createTestRpcNetwork("type here your private key from owner address");
 
 module.exports = {
     networks: {
